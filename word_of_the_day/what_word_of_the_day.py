91d8a483df6f9cfdcb40ce8ea68bd686ba193102
Cormac D <cormacDacker@gmail.com>
2023-12-30 13:58:30 -0800

    consolidated

--- word_of_the_day/what_word_of_the_day.py
+++ word_of_the_day/what_word_of_the_day.py
@@ -1,9 +1,51 @@
 import os
-import wotd
 from inky import InkyWHAT
 from PIL import Image, ImageFont, ImageDraw
 from font_source_sans_pro import SourceSansProSemibold
 from font_fredoka_one import FredokaOne
+import requests
+from bs4 import BeautifulSoup
+
+# this script gets the word of the day
+url = 'https://www.dictionary.com/e/word-of-the-day/'
+raw = requests.get(url)
+soup = BeautifulSoup(raw.content, 'html.parser')
+
+
+# get word of the day
+def get_word_of_the_day():
+    word = soup.find('h1', class_="js-fit-text").text
+    return word
+
+
+# get pronunciation
+def get_pronunciation():
+    pronunciation = soup.find('span',
+                              class_="otd-item-headword__pronunciation__text").text
+    # remove brackets and empty space
+    pronunciation = pronunciation.replace('[', '').replace(']', '').replace(' ', '').replace('\n', '')
+    return pronunciation
+
+
+# get part of speach
+def get_part_of_speach():
+    part_of_speach = soup.find('span', class_="luna-pos").text
+    return part_of_speach
+
+# get definition
+def get_definition():
+    definition = soup.find('div', class_="otd-item-headword__pos-blocks").text
+
+    definition = definition.split('\n', 1)[1].replace(get_part_of_speach(), '').replace('\n', '')
+
+    # find the first non blank space and remove everything before it
+    for i in range(0, len(definition)):
+        if definition[i] != ' ':
+            definition = definition[i:]
+            break
+
+    return definition
+
 
 def creat_wotd_image(inky_display, color="black"):
     inky_display.set_border(inky_display.WHITE)
@@ -20,10 +62,10 @@ def creat_wotd_image(inky_display, color="black"):
     root = os.path.dirname(os.path.realpath("what_word_of_the_day.py"))
 
     # 1. get the word of the day, pronunciation, and definition
-    word = wotd.get_word_of_the_day().title()
-    pronunciation = '[' + wotd.get_pronunciation() + ']'  # add brackets around pronunciation
-    part_of_speach = wotd.get_part_of_speach()
-    definition = wotd.get_definition()
+    word = get_word_of_the_day().title()
+    pronunciation = '[' + get_pronunciation() + ']'  # add brackets around pronunciation
+    part_of_speach = get_part_of_speach()
+    definition = get_definition()
 
     # 2. create the image to display, Word large at the top, then pronunciation, then definition
     ## 2.1. Word
