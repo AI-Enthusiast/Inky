df2c22d9ef52f68bac472f214d1d50d092eb0d33
Cormac D <cormacDacker@gmail.com>
2023-12-30 15:20:07 -0800

    fix

--- weather/test_weather.py
+++ weather/test_weather.py
@@ -1,15 +1,5 @@
 from inky import InkyWHAT
 from what_weather import create_image as create_weather_image
-import argparse
-
-# color options:
-DISPLAY_COLORS = ["black", "red", "yellow"]
-parser = argparse.ArgumentParser()
-parser.add_argument('--color', '-c', type=str, required=False, choices=DISPLAY_COLORS, help="Display colour")
-color = parser.parse_args().color
-if color is None:
-    color = "black"
-
 inky_display = InkyWHAT(color)
 img = create_weather_image(inky_display)
 img = img.rotate(180)  # flip the image so it's right side up
