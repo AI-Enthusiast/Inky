39ea7aac342f56e46c381110155de07e8eb448e5
Cormac D <cormacDacker@gmail.com>
2023-12-31 11:21:19 -0800

    quotes

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@ from inky import InkyWHAT
 from news import display_news
 from word_of_the_day import display_wotd
 from deviant_art import display_deviation
+from usb_slideshow import display_slideshow
 from weather import display_weather
 import argparse
 import random
@@ -18,7 +19,8 @@ if color is None:
 inky_display = InkyWHAT(color)
 
 # pick at random a news or word of the day
-choices = {"news": display_news, "wotd": display_wotd, "deviation": display_deviation, "weather": display_weather}
+choices = {"news": display_news, "wotd": display_wotd, "deviation": display_deviation,
+           "weather": display_weather, "slideshow": display_slideshow}
 choice = random.choice(list(choices.keys()))
 choices[choice](inky_display, color)
 
