943e76d242ed6132d4dfc54925b6b7d6a2aa9090
Cormac D <cormacDacker@gmail.com>
2023-12-30 17:06:16 -0800

    new logging didn't work

--- bin/config_yellow_inky.sh
+++ bin/config_yellow_inky.sh
@@ -9,15 +9,11 @@ git checkout
 git pull
 
 # Initiate logging
-run_date = run_date="$(date +'%Y-%m-%d')"
-# make a directory for the logs for the day
-mkdir ~/Code/Inky/log/${run_date}
-run_datetime="$(date +'%Y-%m-%d--%H-%M')"
-exec 2>> ~/home/pi/Code/Inky/log/${run_date}/logfile_parent_${run_date}.log
+run_date="$(date +'%Y-%m-%d--%H-%M')"
+exec 2>> ~/home/pi/Code/Inky/log/logfile_parent_${run_date}.log
 
 # Run the Python script and log the job run
-time python3 __init__.py --color yellow 1>> ~/Code/Inky/log/${run_date}/logfile_${run_date}.log 2>> ~/Code/Inky/log/${run_date}/errfile_${run_date}.log
-
+time python3 __init__.py --color yellow 1>> ~/Code/Inky/log/logfile_${run_date}.log 2>> ~/Code/Inky/log/errfile_${run_date}.log
 
 # run the script
 # python3 __init__.py --color yellow
